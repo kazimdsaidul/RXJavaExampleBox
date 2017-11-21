0968b4534b8d6cc0ed1aa79608da56621c94f40a
Kazi Md. Saidul <kazis@springrainit.com>
2017-11-21 16:14:29 +0600

    do tow api call with rxjava

--- app/src/main/java/com/example/lenovo/rxjavatwoapicallexample/MainActivity.java
+++ app/src/main/java/com/example/lenovo/rxjavatwoapicallexample/MainActivity.java
@@ -10,8 +10,16 @@ import com.example.lenovo.rxjavatwoapicallexample.model.respose.Respose;
 import com.example.lenovo.rxjavatwoapicallexample.model.webservics.APIService;
 import com.example.lenovo.rxjavatwoapicallexample.model.webservics.ApiInterface;
 
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiFunction;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.Subject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,19 +30,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        multipleAPICall();
+
+
+    }
+
+    private void multipleAPICall() {
+        // request object
         Body body = new Body();
         body.setAmountToCheck("11");
-//        body.setToken("241785278");
-
+        body.setToken("241785278");
 
         RequestBody requestBody = new RequestBody("x5gb5smmaa4d9n4jf8ghnkc9v45bppz6", "au-accounts", "Xaw287-3", "balance");
         requestBody.setBody(body);
 
 
-        APIService.getRetrofitClient().create(ApiInterface.class).getAmount(requestBody)
-                .subscribeOn(Schedulers.io())
+        // start http calling
+        Observable.zip(APIService.getRetrofitClient().create(ApiInterface.class).getAmount(requestBody),
+                APIService.getRetrofitClient().create(ApiInterface.class).getAmount(requestBody),
+                (respose, respose2) -> {
+                    Log.e(TAG, "apply: ");
+
+                    // two request response call
+                    // then conbin on object
+
+                    return respose;
+                }).
+                subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(this::handleResults, this::handleError);
+        .subscribe(this::handleResults);
+
 
 
     }
